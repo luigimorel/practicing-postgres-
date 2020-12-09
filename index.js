0ed75c836b24cc8beb34b06b8c1115ab99b03f17
morelmiles <morelmiles@gmail.com>
2020-12-09 16:54:48 +0300

    Add db connection

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const Sequelize = require("sequelize");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) =>res.json({message: 'Hello'}))
+//Connect to the database
+const sequelize = new Sequelize("postgres://user:pass@example.com:5432/morel");
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been set succesfully");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the DB", +err);
+  });
+
+// User table
+const User = sequelize.define(
+  "user",
+  {
+    //Attributes
+    firstName: { type: Sequelize.STRING, allowNull: false },
+
+    lastName: { type: Sequelize.STRING, allowNull: false },
+  },
+  {
+    //Options
+  }
+);
+
+// Add a new user to the DB
+app.post("/user", (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    newUser.save();
+    res.json({ user: newUser }); //Return the new user create in the data base
+  } catch (error) {
+    console.error(error);
+  }
+});
+User.sync({ force: true });
+app.get("/", (req, res) => res.json({ message: "Hello" }));
 
 const port = 3000;
 
-app.listen(port, ()=>console.log(`App is running on port ${port}`))
+app.listen(port, () => console.log(`App is running on port ${port}`));
